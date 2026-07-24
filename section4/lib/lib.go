de733fa675d48d660251bb3d89e9c493a7f1b093
coli <kkwoo001021@naver.com>
2026-07-25 03:05:08 +0900

    chore: 접근제어자

---
+++ section4/lib/lib.go
@@ -0,0 +1,6 @@
+// 라이브러리 및 접근제어(1)
+package lib
+
+func CheckNum1(c int32) bool {
+	return c > 10
+}
--- section4/pakage2.go
+++ section4/pakage2.go
@@ -11,6 +11,6 @@ func main() {
 	//1.메인 프로그램(main)
 	//2.다른 패키지에서 호출 가능한 라이브러리
 
-	fmt.Print("10 보다 큰 수? :  ", lib.CheckNum(15))
+	fmt.Print("10 보다 큰 수? :  ", lib.CheckNum1(15))
 
 }
