d332de6bf443b52fd29b644ac19eb3ac6bd8cfa6
coli <kkwoo001021@naver.com>
2026-08-09 01:29:52 +0900

    chore: 채널 고급 1

---
+++ section9/gochannel_ex3.go
@@ -0,0 +1,43 @@
+//채널(Channel) 심화(3)
+
+package main
+
+import "fmt"
+
+// tot 채널을 close로 닫으면 어떻게 tot를 이용하는 후속로직에서 수신을 하지?
+func receiveOnly(cnt int) <-chan int {
+	sum := 0
+	tot := make(chan int)
+	go func() {
+		for i := 1; i <= cnt; i++ {
+			sum += i
+		}
+		tot <- sum
+		tot <- 777
+		tot <- 777
+		close(tot)
+	}()
+	return tot
+}
+
+func total(c <-chan int) <-chan int {
+	tot := make(chan int)
+	go func() {
+		a := 0
+		for i := range c {
+			a = a + i
+		}
+		tot <- a
+	}()
+	return tot
+}
+
+func main() {
+	//채널(Channel)
+
+	//예제1
+	c := receiveOnly(100) //채널 반환
+	output := total(c)    //채널 전달 후 반환
+	//output <- 777         //예외
+	fmt.Println("ex1 : ", <-output)
+}
